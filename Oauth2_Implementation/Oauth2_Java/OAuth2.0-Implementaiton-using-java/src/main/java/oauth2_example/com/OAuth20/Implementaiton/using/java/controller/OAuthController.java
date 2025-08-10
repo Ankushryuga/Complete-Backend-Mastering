59ac3d6aa58993aa20ab71330316773ef8c7f2c7
Ankushryuga <ar2609034@gmail.com>
2025-08-10 22:35:02 +0530

    Oauth2 with java implementation

---
+++ Oauth2_Implementation/Oauth2_Java/OAuth2.0-Implementaiton-using-java/src/main/java/oauth2_example/com/OAuth20/Implementaiton/using/java/controller/OAuthController.java
@@ -0,0 +1,4 @@
+package oauth2_example.com.OAuth20.Implementaiton.using.java.controller;
+
+public class OAuthController {
+}
---
+++ Oauth2_Implementation/Oauth2_Java/OAuth2.0-Implementaiton-using-java/src/main/java/oauth2_example/com/OAuth20/Implementaiton/using/java/util/PKCEUtil.java
@@ -0,0 +1,4 @@
+package oauth2_example.com.OAuth20.Implementaiton.using.java.util;
+
+public class PKCEUtil {
+}
