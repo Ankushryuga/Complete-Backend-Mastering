d7686cc130cf402b6e7599b7d051f7d18272e34e
Ankushryuga <ar2609034@gmail.com>
2025-08-10 22:35:18 +0530

    Oauth2

--- Oauth2_Implementation/Oauth2_Java/OAuth2.0-Implementaiton-using-java/src/main/java/oauth2_example/com/OAuth20/Implementaiton/using/java/config/RedisConfig.java
+++ Oauth2_Implementation/Oauth2_Java/OAuth2.0-Implementaiton-using-java/src/main/java/oauth2_example/com/OAuth20/Implementaiton/using/java/config/RedisConfig.java
@@ -1,4 +1,21 @@
 package oauth2_example.com.OAuth20.Implementaiton.using.java.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
 public class RedisConfig {
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(){
+        return new LettuceConnectionFactory();
+    }
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory){
+        RedisTemplate<String, String> template=new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        return template;
+    }
 }
--- Oauth2_Implementation/Oauth2_Java/OAuth2.0-Implementaiton-using-java/src/main/java/oauth2_example/com/OAuth20/Implementaiton/using/java/util/PKCEUtil.java
+++ Oauth2_Implementation/Oauth2_Java/OAuth2.0-Implementaiton-using-java/src/main/java/oauth2_example/com/OAuth20/Implementaiton/using/java/util/PKCEUtil.java
@@ -1,4 +1,24 @@
 package oauth2_example.com.OAuth20.Implementaiton.using.java.util;
 
+import java.security.MessageDigest;
+import java.security.SecureRandom;
+import java.util.Base64;
+
 public class PKCEUtil {
+    private static final SecureRandom secureRandom=new SecureRandom();
+    public static String generateCodeVerifier(int length){
+        byte[] bytes=new byte[length];
+        secureRandom.nextBytes(bytes);
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(bytes);
+    }
+
+    public static String generateCodeChallenge(String verifier){
+        try{
+            MessageDigest md=MessageDigest.getInstance("SHA-256");
+            byte[] digest=md.digest(verifier.getBytes("US-ASCII"));
+            return Base64.getUrlEncoder().withoutPadding().encodeToString(digest);
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
 }
